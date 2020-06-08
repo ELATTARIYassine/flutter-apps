5c6cbe7fd7667fc4c81941f2dd45b66cce2e8d37
El attari Yassine <yassineattari64@gmail.com>
2020-06-08 19:35:42 +0100

    [budget_app] feat: well, i finished the app I dont even remember what i did

--- budget_app/lib/main.dart
+++ budget_app/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:budgetapp/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:budgetapp/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Budget UI',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.green,
